be30e0026181438d2a8d1f45b97318e32cf7c74e
Cathy Wei <cathy.wei@zebra.com>
2020-04-16 15:33:28 -0400

    DataWedge Sample app

---
+++ MultiActivity/app/src/main/java/com/zebra/dwmultiactivity/MainActivity.java
@@ -0,0 +1,196 @@
+package com.zebra.dwmultiactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String PROFILE1 = "MultiActivity_Profile1";
+    private static final String PROFILE2 = "MultiActivity_Profile2";
+
+    IntentFilter filter = new IntentFilter();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSwitchActivity = findViewById(R.id.btnGoToSecondActivity);
+        btnSwitchActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switchActivity = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(switchActivity);
+            }
+        });
+
+        filter.addAction(DataWedgeInterface.ACTION_RESULT_DATAWEDGE);
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+        filter.addAction(DataWedgeInterface.ACTIVITY_INTENT_FILTER_ACTION);  // The filtered action must match the "Intent action" specified in the DW Profile's Intent output configuration
+
+        // Create Profile 1 for MainActivity: Enable Code128, Disable EAN13
+        String Code128Value = "true";
+        String EAN13Value = "false";
+        CreateProfile(PROFILE1, Code128Value, EAN13Value);
+
+        // Create Profile 2 for SecondActivity: Disable Code128, Enable EAN13
+        Code128Value = "false";
+        EAN13Value = "true";
+        CreateProfile(PROFILE2, Code128Value, EAN13Value);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+        registerReceiver(myBroadcastReceiver, filter);
+
+        // Retrieve current active profile using GetActiveProfile: http://techdocs.zebra.com/datawedge/latest/guide/api/getactiveprofile/
+        DataWedgeInterface.sendDataWedgeIntentWithExtra(getApplicationContext(),
+                DataWedgeInterface.ACTION_DATAWEDGE, DataWedgeInterface. EXTRA_GET_ACTIVE_PROFILE,
+                DataWedgeInterface.EXTRA_EMPTY);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+        unregisterReceiver(myBroadcastReceiver);
+    }
+
+    // Used EventBus to notify foreground activity of profile change
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(DataWedgeInterface.MessageEvent event) {
+        TextView txtActiveProfile = findViewById(R.id.txtActiveProfile);
+        txtActiveProfile.setText(event.activeProfile);
+    };
+
+    // Receive scanned data
+    private BroadcastReceiver myBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // Bundle b = intent.getExtras();
+
+            //  This is useful for debugging to verify the format of received intents from DataWedge
+            //for (String key : b.keySet())
+            //{
+            //    Log.v("MultiActivity", key);
+            //}
+
+            if (action.equals(DataWedgeInterface.ACTION_RESULT_DATAWEDGE))
+            {
+                if (intent.hasExtra(DataWedgeInterface.EXTRA_RESULT_GET_ACTIVE_PROFILE))
+                {
+                    String activeProfile = intent.getStringExtra(DataWedgeInterface.EXTRA_RESULT_GET_ACTIVE_PROFILE);
+                    EventBus.getDefault().post(new DataWedgeInterface.MessageEvent(activeProfile));
+                }
+            }
+
+            if (action.equals(DataWedgeInterface.ACTIVITY_INTENT_FILTER_ACTION)) {
+                //  Received a barcode scan
+                try {
+                    displayScanResult(intent, "via Broadcast");
+                } catch (Exception e) {
+                    //  Catch if the UI does not exist when we receive the broadcast
+                    Toast.makeText(getApplicationContext(), "Error; " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    };
+
+    // Display scanned data
+    private void displayScanResult(Intent initiatingIntent, String howDataReceived)
+    {
+        String decodedData = initiatingIntent.getStringExtra(DataWedgeInterface.DATAWEDGE_INTENT_KEY_DATA);
+        String decodedDecoder = initiatingIntent.getStringExtra(DataWedgeInterface.DATAWEDGE_INTENT_KEY_DECODER);
+
+        final TextView lblScanData = (TextView) findViewById(R.id.lblScanData);
+        final TextView lblScanDecoder = (TextView) findViewById(R.id.lblScanDecoder);
+
+        lblScanData.setText(decodedData);
+        lblScanDecoder.setText(decodedDecoder);
+    }
+
+    private void CreateProfile (String profileName, String code128Value, String ean13Value){
+
+        // Configure profile to apply to this app
+        Bundle bMain = new Bundle();
+        bMain.putString("PROFILE_NAME", profileName);
+        bMain.putString("PROFILE_ENABLED", "true");
+        bMain.putString("CONFIG_MODE", "CREATE_IF_NOT_EXIST");  // Create profile if it does not exist
+
+        // Configure barcode input plugin
+        Bundle bConfigBarcode = new Bundle();
+        bConfigBarcode.putString("PLUGIN_NAME", "BARCODE");
+        bConfigBarcode.putString("RESET_CONFIG", "true"); //  This is the default
+
+        // PARAM_LIST bundle properties
+        Bundle bParamsBarcode = new Bundle();
+        bParamsBarcode.putString("scanner_selection", "auto");
+        bParamsBarcode.putString("scanner_input_enabled", "true");
+        bParamsBarcode.putString("decoder_code128", code128Value);
+        bParamsBarcode.putString("decoder_ean13", ean13Value);
+
+        // Bundle "bParamsBarcode" within bundle "bConfigBarcode"
+        bConfigBarcode.putBundle("PARAM_LIST", bParamsBarcode);
+
+        // Associate appropriate activity to profile
+        String activityName;
+        Bundle appConfig = new Bundle();
+        appConfig.putString("PACKAGE_NAME", getPackageName());
+        if (profileName.equals(PROFILE1))
+        {
+            activityName = MainActivity.class.getSimpleName();
+        }
+        else {
+            activityName = SecondActivity.class.getSimpleName();
+        }
+        String activityPackageName = getPackageName() + "." + activityName;
+        appConfig.putStringArray("ACTIVITY_LIST", new String[] {activityPackageName});
+        bMain.putParcelableArray("APP_LIST", new Bundle[]{appConfig});
+
+        // Configure intent output for captured data to be sent to this app
+        Bundle bConfigIntent = new Bundle();
+        bConfigIntent.putString("PLUGIN_NAME", "INTENT");
+        bConfigIntent.putString("RESET_CONFIG", "true");
+
+        // Set params for intent output
+        Bundle bParamsIntent = new Bundle();
+        bParamsIntent.putString("intent_output_enabled", "true");
+        bParamsIntent.putString("intent_action", "com.zebra.dwmultiactivity.ACTION");
+        bParamsIntent.putString("intent_delivery", "2");
+
+        // Bundle "bParamsIntent" within bundle "bConfigIntent"
+        bConfigIntent.putBundle("PARAM_LIST", bParamsIntent);
+
+        // Place both "bConfigBarcode" and "bConfigIntent" bundles into arraylist bundle
+        ArrayList<Bundle> bundlePluginConfig = new ArrayList<>();
+        bundlePluginConfig.add(bConfigBarcode);
+        bundlePluginConfig.add(bConfigIntent);
+
+        // Place bundle arraylist into "bMain" bundle
+        bMain.putParcelableArrayList("PLUGIN_CONFIG", bundlePluginConfig);
+
+        // Apply configs using SET_CONFIG: http://techdocs.zebra.com/datawedge/latest/guide/api/setconfig/
+        DataWedgeInterface.sendDataWedgeIntentWithExtra(getApplicationContext(),
+                DataWedgeInterface.ACTION_DATAWEDGE, DataWedgeInterface.EXTRA_SET_CONFIG, bMain);
+
+        Toast.makeText(getApplicationContext(), "Created profiles.  Check DataWedge app UI.", Toast.LENGTH_LONG).show();
+    }
+}
